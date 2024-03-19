d6f744314ea3e408c9375cf6f1127272f5130154
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-03-19 19:56:38 +0000

    Implement change password page

---
+++ change-password.js
@@ -0,0 +1,20 @@
+function showChangePasswordSuccessOrError() {
+    document.getElementById('change-password').style.display = 'none';
+    // document.getElementById('change-password-successful').style.display = 'none';
+    // document.getElementById('change-password-successful').style.display = 'none';
+
+    let x=2;
+    if (x == 1) {
+        document.getElementById('change-password-successful').style.display = 'block';
+        document.getElementById('change-password-error').style.display = 'none';
+    } else {
+        document.getElementById('change-password-successful').style.display = 'none';
+        document.getElementById('change-password-error').style.display = 'block';
+    }
+}
+
+function showChangePassword() {
+    document.getElementById('change-password').style.display = 'block';
+    document.getElementById('change-password-successful').style.display = 'none';
+    document.getElementById('change-password-error').style.display = 'none';
+}
