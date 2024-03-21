60f1b7ee6412fce8e5d9d3915031fe5fb61aeeba
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-03-21 12:12:24 +0000

    Implement tab functionality on user voter page

--- script.js
+++ script.js
@@ -27,23 +27,3 @@ class Footer extends HTMLElement {
 }
 
 customElements.define('page-footer', Footer);
-
-class UserPageHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-            <div class="header">
-                <div class="logo"><a href="/index.html" target="_blank"><span class="fi fi-ng"></span> NEEV</a></div>
-                <nav>
-                <ul>
-                    <li><a href="/index.html">Dashboard</a></li>
-                    <li><a href="/admin/admin.html">Elections</a></li>
-                    <li><a href="/user/user.html">Election Results</a></li>
-                </ul>
-                </nav>
-                <Button>Logout</Button>
-            </div>
-        `;
-    }
-}
-
-customElements.define('userpage-header', UserPageHeader);
--- user/user-dashboard.js
+++ user/user-dashboard.js
@@ -89,3 +89,21 @@ document.getElementById('go-to-dashboard-now').onclick = function(){
     verifyPhoneContent.style.display = 'none';
     verifyPhonenoError.style.display = 'none';
 }
+
+// Tab functionality
+function openTab(e, tabTitle){
+    var index, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName('tabcontent');
+    for (index = 0; index < tabContent.length; index++) {
+        tabContent[index].style.display = 'none';
+    }
+
+    tabLinks = document.getElementsByClassName('tablinks');
+    for (index = 0; index < tabLinks.length; index++) {
+        tabLinks[index].className = tabLinks[index].className.replace(" active", "");
+    }
+
+    document.getElementById(tabTitle).style.display = 'block';
+    e.currentTarget.className += " active";
+}
