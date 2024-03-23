adae9f738a3a6a3c018d440c71ad289d38654992
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-03-23 02:27:52 +0000

    Implement functionality for user to vote in two elections using dummy data

--- user/user-dashboard.js
+++ user/user-dashboard.js
@@ -1,4 +1,3 @@
-// const verificationModal = document.getElementById('verification-modal');
 const main = document.getElementsByClassName('main-content')[0];
 const modal = document.getElementsByClassName('modal')[0];
 const verifyEmailContent = document.getElementById('verify-email-content');
@@ -115,4 +114,127 @@ function clickToCopyVotingId() {
     const copyValue = document.getElementById('voting-id').innerHTML;
     navigator.clipboard.writeText(copyValue);
     alert("Copied " + copyValue);
+}
+
+// Dummy data voting simulation
+const voter = {
+    hasVotingId: true,
+    votingId: '232123213',
+    hasVoted: false,
+    votedElections: { 
+        "General1": false,
+        "General2": true, 
+        "General3": false
+    }
+};
+
+// Click Elections
+function goToElections() {
+    if (voter.hasVotingId) {
+        showStep('nevs-choose-language')
+    } else {
+        showStep('nevs-profile-not-verified')
+    }
+}
+
+// Profile is not verified, go back to election page
+function goToElectionPage() {
+    showStep('nevs-start-election-page')
+}
+
+//After choosing a language, enter Voting ID
+function enterVotingId() {
+    showStep('nevs-enter-voting-id')
+}
+
+// Validate Voting ID
+function validateVotingId() {
+    var enteredVotingId = document.getElementById('votingID').value;
+
+    if (enteredVotingId == voter.votingId) {
+        showStep('nevs-election-conditions')
+    } else {
+        showStep('nevs-voting-id-not-valid')
+    }
+}
+
+// Voting ID is not correct, go back to election page
+function goToElectionPage() {
+    showStep('nevs-start-election-page')
+}
+// Voting ID is not correct, enter voting ID again
+function ReEnterVotingId() {
+    showStep('nevs-enter-voting-id')
+}
+
+// Checkbox confirmation: Agree to conditions and start voting
+document.getElementById('agreeConditions').addEventListener('change', function(){
+    document.getElementById('startVoting').disabled = !this.checked;
+});
+
+// Select election activities
+function startVoting(){
+    showStep('nevs-select-election')
+};
+
+// Select election to participate in
+function confirmElection() {
+    var electionSelected = document.getElementById('electionSelection').value;
+    console.log(electionSelected)
+    handleElectionSelection(electionSelected);
+}
+
+// Check if user has already voted in this election
+function handleElectionSelection(electionSelected) {
+    if (voter.votedElections[electionSelected]) {
+        showStep('nevs-voter-already-voted')
+    } else {
+        if (electionSelected == 'General1') {
+            showStep('nevs-PRESIDENT') // Open Presidential election
+        }
+    }
+};
+
+// Vote for President
+function voteCandidate () {
+    showStep('nevs-confirm-vote');
+}
+
+function changeVote () {
+    showStep('nevs-PRESIDENT');
+}
+
+function goToNextElection () {
+    showStep('nevs-SENATE');
+}
+
+// Vote of Senate
+function voteCandidateSen () {
+    showStep('nevs-confirm-vote-sen');
+}
+
+function changeVoteSen () {
+    showStep('nevs-SENATE');
+}
+
+function votingCompleted() {
+    showStep('nevs-voting-successful')
+}
+
+function goToDashboard() {
+    window.location = '/user/user-dashboard.html';
+}
+
+function goToResults() {
+    window.location = '/election-results/electionresults.html';
+}
+
+function showStep(stepId) {
+    // Hide all steps
+    document.querySelectorAll('div[id^="nevs-"]').forEach((div) => {
+        div.style.display = 'none';
+    });
+
+    // Show the targeted step
+    document.getElementById(stepId).style.display = 'block';
 }
