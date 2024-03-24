f7d50b82e032e8f7b6b2b0294e8808da1043d7b2
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-03-24 14:27:36 +0000

    Implement addParty tab and createElection tab contents on admin page

--- admin/admin-dashboard.js
+++ admin/admin-dashboard.js
@@ -17,3 +17,43 @@ function openTab(e, tabTitle) {
 }
 
 document.getElementById('defaultPage').click()
+
+// ADD PARTY LOGIC
+// Display add party form
+function showAddPartyForm() {
+    document.getElementById('addPartyForm').style.display = 'block';
+}
+
+// Display party added successfully
+function addParty() {
+    document.getElementById('addPartyForm').style.display = 'none';
+    document.getElementById('partyAddedSuccess').style.display = 'block';
+    document.getElementById('addPartyButton').style.display = 'none';
+}
+
+// When 'Ok' is clicked on party added successful card, go back to add party page
+function goToAddParty() {
+    document.getElementById('addPartyButton').style.display = 'block';
+    document.getElementById('addPartyForm').style.display = 'none';
+    document.getElementById('partyAddedSuccess').style.display = 'none';
+}
+
+// CREATE ELECTION LOGIC
+// Display create election form
+function showCreateElectionForm() {
+    document.getElementById('createElectionForm').style.display = 'block';
+}
+
+// Display party added successfully
+function createElection() {
+    document.getElementById('createElectionForm').style.display = 'none';
+    document.getElementById('electionCreatedSuccess').style.display = 'block';
+    document.getElementById('createElectionTag').style.display = 'none';
+}
+
+// When 'Ok' is clicked on party added successful card, go back to add party page
+function goToCreateElection() {
+    document.getElementById('createElectionForm').style.display = 'none';
+    document.getElementById('electionCreatedSuccess').style.display = 'none';
+    document.getElementById('createElectionTag').style.display = 'block';
+}
